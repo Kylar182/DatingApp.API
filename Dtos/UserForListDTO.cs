3c3075a0df7a2659301e0a03ecf501748e048866
Unknown <ajbeeman@kfsoftwaresolutions.com>
2020-02-05 18:39:29 -0600

    Snip Snip

--- Dtos/UserForListDTO.cs
+++ Dtos/UserForListDTO.cs
@@ -22,33 +22,5 @@ namespace DatingApp.API.Dtos
     public string StateProv { get; set; }
     public string CountryId { get; set; }
     public string PhotoURL { get; set; }
-
-    public UserForListDTO() { }
-
-    public UserForListDTO(User usr)
-    {
-      Username = usr.Username;
-      FirstName = usr.FirstName;
-      LastName = usr.LastName;
-      Gender = usr.Gender;
-      Age = usr.DateOfBirth.GetAge();
-      KnownAs = usr.KnownAs;
-      Created = usr.Created;
-      LastActive = usr.LastActive;
-      City = usr.City;
-      StateProv = usr.StateProv;
-      CountryId = usr.CountryId;
-      PhotoURL = usr.Photos.Where(im => im.IsMain == true).Select(url => url.Url).First();
-    }
-
-    public static IEnumerable<UserForListDTO> BuildList(IEnumerable<User> users)
-    {
-      var list = new List<UserForListDTO>();
-
-      foreach(var user in users)
-        list.Add(new UserForListDTO(user));
-
-      return list;
-    }
   }
 }
