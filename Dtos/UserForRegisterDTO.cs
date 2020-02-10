35d5243df572e825feb30f0a48e47b3326d0a856
Unknown <ajbeeman@kfsoftwaresolutions.com>
2020-02-10 05:50:59 -0600

    WIP: 12-27

--- Dtos/UserForRegisterDTO.cs
+++ Dtos/UserForRegisterDTO.cs
@@ -7,9 +7,9 @@ namespace DatingApp.API.Dtos
   public class UserForRegisterDTO
   {
     [StringLength(35, MinimumLength = 6, ErrorMessage = "You must specify a username between 6 and 35 characters")]
-    [RegularExpression(@"^[A-Za-z.\s_-]+$", ErrorMessage = "Username can only contain uppercase/lowercase, periods, spaces and dashes")]
+    [RegularExpression(@"^[A-Za-z0-9.\s_-]+$", ErrorMessage = "Username can only contain uppercase/lowercase, periods, spaces and dashes")]
     public string Username { get; set; }
-    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,35}$", ErrorMessage = "Password must contain at least 1 Uppercase letter, 1 Lowercase letter, 1 special character, and 1 number")]
+    [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$", ErrorMessage = "Password must contain at least 1 Uppercase letter, 1 Lowercase letter, 1 special character, and 1 number")]
     public string Password { get; set; }
 
     [StringLength(35, MinimumLength = 2, ErrorMessage = "You must specify a first name between 2 and 35 characters")]
